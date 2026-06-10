099a24879f475a8dd7043c47b3e1423d035ce86b
Gaurav Sharma <sharmag8121@gmail.com>
2026-06-11 00:14:07 +0530

    backend initial commit

---
+++ backend/src/test/java/com/taskapp/backend/TaskappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskapp.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
